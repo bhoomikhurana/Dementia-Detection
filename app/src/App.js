13dc0d97a2b427acb8fb3ea730af135e26d1b47c
dhirajkadam27 <95651199+dhirajkadam27@users.noreply.github.com>
2023-11-11 11:39:21 +0530

    Update App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,18 +1,32 @@
-import logo from './logo.svg';
+// App.js
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/Main';
 import Feature from './components/Feature';
 import Contact from './components/Contact';
-
+import LoginPage from './components/LoginPage';
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    // Set the login state to true when the user successfully logs in
+    setLoggedIn(true);
+  };
+
   return (
     <>
-      <Navbar/>
-      <Main/>
-      <Feature/>
-      <Contact/>
+      {loggedIn ? (
+        <>
+          <Navbar />
+          <Main />
+          <Feature />
+          <Contact />
+        </>
+      ) : (
+        <LoginPage onLogin={handleLogin} />
+      )}
     </>
   );
 }
